3c9f812cb2e836e496299eb3375b2c569df98be6
Cody Washington <115746975+Jelatinone@users.noreply.github.com>
2025-10-09 22:43:09 -0500

    Tweak PageHead for Mobile

--- src/pages/home/sections/HomePublicationsSection.jsx
+++ src/pages/home/sections/HomePublicationsSection.jsx
@@ -31,7 +31,7 @@ export default function HomePublicationsSection() {
         </article>
         <Carousel className="h-full border-t-2 md:border-t-0 md:border-l-2 w-full  py-5 md:pl-10 border-neutral-400">
           {Array.from({ length: total }).map((_, i) => (
-            <CarouselImage key={i} src="" alt="" date={new Date()}/>
+            <CarouselImage key={i} src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/PNG_Test.png/960px-PNG_Test.png?20250623065344" alt="" date={new Date()}/>
           ))}
         </Carousel>
       </div>
--- src/pages/serve/ServePage.jsx
+++ src/pages/serve/ServePage.jsx
@@ -1,3 +1,14 @@
+import PageHeader from "../PageHeader";
+
 export default function SavePage() {
-	
+  return (
+    <PageHeader src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/PNG_Test.png/960px-PNG_Test.png?20250623065344">
+      <h2 className="font-urbanist font-bold text-neutral-50 text-xxlg md:text-xxxlg">
+        SERVE
+      </h2>
+      <p className="font-semibold text-neutral-200 text-sm pb-10">
+        GET INVOLVED IN YOUR COMMUNITY
+      </p>
+    </PageHeader>
+  );
 }
