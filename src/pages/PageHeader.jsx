b87d53da0ea935a722cc185b69a8e735b6d0254d
Cody Washington <115746975+Jelatinone@users.noreply.github.com>
2025-11-07 09:58:41 -0600

    Inline Mask

--- src/pages/PageHeader.jsx
+++ src/pages/PageHeader.jsx
@@ -3,16 +3,14 @@ export default function PageHeader({ children, className }) {
     <section
       className={`w-full md:justify-items-left items-center relative flex my-0 mb-10 h-85 md:h-70 ${className}`}>
       <img
-        className="absolute inset-0 z-0 h-full w-full object-cover brightness-[.65] contrast-[1.1]"
-        style={{
-          WebkitMaskImage:
-            "linear-gradient(to bottom, white 65%, transparent 100%)",
-          maskImage: "linear-gradient(to bottom, white 65%, transparent 100%)",
-        }}
+        className="absolute inset-0 z-0 h-full w-full object-cover brightness-[.65] contrast-[1.1]
+        [mask-image:linear-gradient(to_bottom,white_65%,transparent_100%)] [webkit-mask-image:linear-gradient(to_bottom,white_65%,transparent_100%)]
+        md:[mask-image:linear-gradient(to_bottom,white_70%,transparent_100%)] md:[webkit-mask-image:linear-gradient(to_bottom,white_70%,transparent_100%)]
+        "
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/PNG_Test.png/960px-PNG_Test.png?20250623065344"
       />
       <div
-        className={`absolute z-10 w-fit self-end top-[59%] grid px-5 lg:px-35 items-start ${className}`}>
+        className={`absolute z-10 w-fit self-end top-[59%] md:top-[52%] grid px-5 lg:px-35 items-start ${className}`}>
         {children}
       </div>
     </section>
