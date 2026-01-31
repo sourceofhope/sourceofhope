fa715018e28144155479ebd507a51dcd1c38eb8a
Cody Washington <115746975+Jelatinone@users.noreply.github.com>
2026-01-31 13:26:07 -0600

    Try Host

--- src/lib/api/client.js
+++ src/lib/api/client.js
@@ -3,7 +3,7 @@ const hostname = window.location.hostname;
 const API_BASE_URL =
   hostname === "localhost" || hostname === "127.0.0.1"
     ? "http://localhost:3001/api"
-    : hostname.includes("dev.")
+    : window.location.host.includes("dev.")
       ? "https://api.thesourceofhope.org/dev/api"
       : "https://api.thesourceofhope.org/app/api";
 
