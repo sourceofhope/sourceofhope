5b5a8fb72ee378a66a7c3545896c8f7718e9f45b
Cody Washington <115746975+Jelatinone@users.noreply.github.com>
2026-02-07 10:14:33 -0600

    Member Page Consistency

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,6 @@ import ErrorBoundary from "./pages/ErrorBoundary.jsx";
 import HomePage from "./pages/home/HomePage.jsx";
 import AboutPage from "./pages/about/AboutPage.jsx";
 import TeamPage from "./pages/about/TeamPage.jsx";
-import TeamMemberBioPage from "./pages/about/TeamMemberBioPage.jsx";
 import ServePage from "./pages/serve/ServePage.jsx";
 import ConnectPage from "./pages/connect/ConnectPage.jsx";
 import MediaPage from "./pages/media/MediaPage.jsx";
@@ -30,98 +29,99 @@ import ProductPage from "./pages/storefront/ProductPage.jsx";
 import PodcastPage from "./pages/media/PodcastPage.jsx";
 import PressPage from "./pages/media/PressPage.jsx";
 import { HelmetProvider } from "react-helmet-async";
+import MemberPage from "./pages/about/MemberPage.jsx";
 
 export const router = createBrowserRouter([
-	{
-		path: CANONICAL.home.absolute,
-		element: <AppLayout />,
-		errorElement: <ErrorBoundary />,
-		children: [
-			{ index: true, element: <HomePage /> },
+  {
+    path: CANONICAL.home.absolute,
+    element: <AppLayout />,
+    errorElement: <ErrorBoundary />,
+    children: [
+      { index: true, element: <HomePage /> },
 
-			{
-				path: CANONICAL.about.relative,
-				element: <AboutPage />,
-			},
-			{
-				path: CANONICAL.about.team.relative,
-				element: <TeamPage />,
-			},
-			{
-				path: CANONICAL.about.teamMember.relative,
-				element: <TeamMemberBioPage />,
-			},
+      {
+        path: CANONICAL.about.relative,
+        element: <AboutPage />,
+      },
+      {
+        path: CANONICAL.about.team.relative,
+        element: <TeamPage />,
+      },
+      {
+        path: CANONICAL.about.member.relative,
+        element: <MemberPage />,
+      },
 
-			{
-				path: CANONICAL.serve.relative,
-				element: <ServePage />,
-			},
-			{
-				path: CANONICAL.serve.servingHope.relative,
-				element: <ServingHopeProgram />,
-			},
-			{
-				path: CANONICAL.serve.educationHope.relative,
-				element: <EducationHopeProgram />,
-			},
-			{
-				path: CANONICAL.serve.wellnessHope.relative,
-				element: <WellnessHopeProgram />,
-			},
-			{
-				path: CANONICAL.serve.outdoorHope.relative,
-				element: <OutdoorHopeProgram />,
-			},
-			{
-				path: CANONICAL.serve.internationalHope.relative,
-				element: <InternationalHopeProgram />,
-			},
+      {
+        path: CANONICAL.serve.relative,
+        element: <ServePage />,
+      },
+      {
+        path: CANONICAL.serve.servingHope.relative,
+        element: <ServingHopeProgram />,
+      },
+      {
+        path: CANONICAL.serve.educationHope.relative,
+        element: <EducationHopeProgram />,
+      },
+      {
+        path: CANONICAL.serve.wellnessHope.relative,
+        element: <WellnessHopeProgram />,
+      },
+      {
+        path: CANONICAL.serve.outdoorHope.relative,
+        element: <OutdoorHopeProgram />,
+      },
+      {
+        path: CANONICAL.serve.internationalHope.relative,
+        element: <InternationalHopeProgram />,
+      },
 
-			{
-				path: CANONICAL.connect.relative,
-				element: <ConnectPage />,
-			},
+      {
+        path: CANONICAL.connect.relative,
+        element: <ConnectPage />,
+      },
 
-			{
-				path: CANONICAL.media.relative,
-				element: <MediaPage />,
-			},
-			{
-				path: CANONICAL.media.podcast.relative,
-				element: <PodcastPage />,
-			},
-			{
-				path: CANONICAL.media.press.relative,
-				element: <PressPage />,
-			},
+      {
+        path: CANONICAL.media.relative,
+        element: <MediaPage />,
+      },
+      {
+        path: CANONICAL.media.podcast.relative,
+        element: <PodcastPage />,
+      },
+      {
+        path: CANONICAL.media.press.relative,
+        element: <PressPage />,
+      },
 
-			{
-				path: CANONICAL.member.relative,
-				element: <FormPage />,
-			},
+      {
+        path: CANONICAL.member.relative,
+        element: <FormPage />,
+      },
 
-			{
-				path: CANONICAL.storefront.relative,
-				element: <StorefrontPage />,
-			},
-			{
-				path: `${CANONICAL.storefront.products.relative}/:slug`,
-				element: <ProductPage />,
-			},
-			{ path: CANONICAL.storefront.cart.relative, element: <CartPage /> },
+      {
+        path: CANONICAL.storefront.relative,
+        element: <StorefrontPage />,
+      },
+      {
+        path: CANONICAL.storefront.product.relative,
+        element: <ProductPage />,
+      },
+      { path: CANONICAL.storefront.cart.relative, element: <CartPage /> },
 
-			{
-				path: CANONICAL.storefront.success.relative,
-				element: <CartSuccessPage />,
-			},
-		],
-	},
+      {
+        path: CANONICAL.storefront.success.relative,
+        element: <CartSuccessPage />,
+      },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<HelmetProvider>
-			<RouterProvider router={router} />
-		</HelmetProvider>
-	</React.StrictMode>,
+  <React.StrictMode>
+    <HelmetProvider>
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  </React.StrictMode>,
 );
