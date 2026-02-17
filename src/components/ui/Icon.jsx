7309370b7295e996ba4781cc9818f9b785c0173a
Cody Washington <115746975+Jelatinone@users.noreply.github.com>
2026-02-17 10:30:14 -0600

    Allow Focusable Icons

--- src/components/ui/Icon.jsx
+++ src/components/ui/Icon.jsx
@@ -2,7 +2,6 @@ export default function Icon({ children, className = "" }) {
   return (
     <div
       aria-hidden="true"
-      focusable="false"
       className={`
         flex items-center justify-center select-none
         ${className}
