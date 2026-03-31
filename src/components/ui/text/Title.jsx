4dd3db3285325e84620a87505927bdb274a2a5da
Cody Washington <115746975+Jelatinone@users.noreply.github.com>
2026-03-31 10:21:15 -0500

    Extrapolate Into PWD, Fix Missing Dependencies

--- src/components/ui/text/Title.jsx
+++
@@ -1,8 +0,0 @@
-export default function Title({ children, className = "" }) {
-  return (
-    <h2
-      className={`font-semibold font-urbanist text-xlg md:text-xxlg ${className}`}>
-      {children}
-    </h2>
-  );
-}
