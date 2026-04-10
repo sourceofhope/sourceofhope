a675353e304d9bd0933e66854df94bf0a8583823
Cody Washington <115746975+Jelatinone@users.noreply.github.com>
2026-04-10 09:01:20 -0500

    Extrapolate Next Project Into PWD

--- src/components/ui/text/Blockquote.jsx
+++
@@ -1,3 +0,0 @@
-export default function Blockquote({ children, className = "" }) {
-  return <p className={` pl-5 py-0 border-l-4 ${className}`}>{children}</p>;
-}
--- src/components/ui/text/Heading.jsx
+++
@@ -1,8 +0,0 @@
-export default function Heading({ children, className = "" }) {
-  return (
-    <h3
-      className={`text-sm md:text-md font-montserrat text-accent-700 font-bold uppercase ${className}`}>
-      {children}
-    </h3>
-  );
-}
