dd2c6f9f249728b8a72137461457ff62689ca273
Cody Washington <115746975+Jelatinone@users.noreply.github.com>
2026-07-15 21:41:59 -0500

    Fix Giving Header Label

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -53,7 +53,7 @@ const getHeaderLinks = (): NavLink[] => [
 //{ label: "CONNECT", href: "/connect" },
 //moved PLANNED GIVING & QUICK DONATE labels from MEMBERS to a label of its own
  {
-   label: "PLANNED GIVING", 
+   label: "GIVING", 
    href: "/giving" ,
    children: [
     { label: "QUICK DONATE", href: "/donate"},
